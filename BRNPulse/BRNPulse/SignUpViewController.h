9f0faace653e83f7675eb525e7e572c5a4ac05a0
Student <student@achiever51.local>
2016-08-30 10:59:54 +0530

    i have creates signup in signupviewcontroller  ok

---
+++ BRNPulse/BRNPulse/SignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignUpViewController.h
+//  BRNPulse
+//
+//  Created by Student on 30/08/16.
+//  Copyright © 2016 Brn InfoTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignUpViewController : UIViewController
+
+@end
